a9db98fdbc843fefb770b88491f2651adc1b04ea
Manfrinmm <matheus_mm123@hotmail.com>
2020-05-08 16:23:20 -0300

    Add Addresses to User

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,8 +1,9 @@
 "use strict";
 /** @typedef {import('@adonisjs/framework/src/Request')} Request */
 /** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
-
 const User = use("App/models/User");
+
+const Database = use("Database");
 
 class UserController {
   /**
@@ -13,8 +14,17 @@ class UserController {
 
   async store({ request }) {
     const data = request.only(["username", "email", "password"]);
+    const addresses = request.input("addresses");
 
-    const user = await User.create(data);
+    const trx = await Database.beginTransaction();
+
+    const user = await User.create(data, trx);
+
+    await user.addresses().createMany(addresses, trx);
+
+    await user.load("addresses");
+
+    await trx.commit();
 
     return user;
   }
